df89bcf9a62251aed7de86ec9498cbfae2894b27
juuzouvlone <vlonereyvetements@mail.ru>
2023-03-30 12:15:41 +0300

    Create selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,13 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+
+arr = [random.randint(0, 10) for _ in range(10)]
+print(selection_sort(arr))
